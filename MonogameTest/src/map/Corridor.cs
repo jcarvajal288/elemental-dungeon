1d2a0dbb6e1d5b23381402b2c188b0aeaa10d049
Max Carvajal <jcarvajal288@gmail.com>
2024-08-25 17:00:06 -0500

    Room inherits from Corridor

--- MonogameTest/src/map/Corridor.cs
+++ MonogameTest/src/map/Corridor.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -7,21 +6,21 @@ using Microsoft.Xna.Framework.Graphics;
 namespace MonogameTest.map;
 
 public class Corridor {
-    private Vector2 _topLeft;
-    private Vector2 _bottomRight;
-    private Terrain _floorTerrain = Terrain.OrcFloor;
-    private Terrain _wallTerrain = Terrain.OrcWall;
+    protected Vector2 TopLeft;
+    protected Vector2 BottomRight;
+    protected Terrain FloorTerrain = Terrain.OrcFloor;
+    protected Terrain WallTerrain = Terrain.OrcWall;
     
-    private readonly Dictionary<Vector2, Tile> _grid;
+    protected Dictionary<Vector2, Tile> Grid;
 
     public Corridor(Vector2 topLeft, Vector2 bottomRight, List<Vector2> tiles, bool isHorizontal) {
-        _topLeft = topLeft;
-        _bottomRight = bottomRight;
-        _grid = new Dictionary<Vector2, Tile>();
+        TopLeft = topLeft;
+        BottomRight = bottomRight;
+        Grid = new Dictionary<Vector2, Tile>();
         
         HashSet<Vector2> positions = RoomDigger.GetTileRegion(topLeft, bottomRight);
         foreach (Vector2 pos in positions) {
-            _grid[pos] = Tile.CreateTileForTerrain(_floorTerrain);
+            Grid[pos] = Tile.CreateTileForTerrain(FloorTerrain);
         }
              
         List<Vector2> wallTiles = isHorizontal switch {
@@ -29,22 +28,24 @@ public class Corridor {
             false => tiles.Where(tile => (int)tile.X == (int)topLeft.X || (int)tile.X == (int)bottomRight.X).ToList(),
         };
         foreach (Vector2 pos in wallTiles) {
-            _grid[pos] = Tile.CreateTileForTerrain(_wallTerrain);
+            Grid[pos] = Tile.CreateTileForTerrain(WallTerrain);
         }
     }
-    
+
+    protected Corridor() { }
+
     public bool ContainsPosition(Vector2 position) {
-        return position.X >= _topLeft.X && position.X <= _bottomRight.X &&
-               position.Y >= _topLeft.Y && position.Y <= _bottomRight.Y;
+        return position.X >= TopLeft.X && position.X <= BottomRight.X &&
+               position.Y >= TopLeft.Y && position.Y <= BottomRight.Y;
     }
     
     public void Draw(SpriteBatch spriteBatch) {
-        foreach ((Vector2 position, Tile tile) in _grid) {
+        foreach ((Vector2 position, Tile tile) in Grid) {
             tile.Draw(spriteBatch, position);
         }
     }
 
     public Tile GetTileAt(Vector2 position) {
-        return _grid[position];
+        return Grid[position];
     }
 }
--- MonogameTest/src/map/Room.cs
+++ MonogameTest/src/map/Room.cs
@@ -5,69 +5,47 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace MonogameTest.map;
 
-public class Room() {
-    private Vector2 _topLeft;
-    private Vector2 _bottomRight;
-    
-    private readonly Dictionary<Vector2, Tile> _grid;
-    private List<Vector2> _doorTiles;
-    private readonly Terrain _floorTerrain;
-    private readonly Terrain _wallTerrain;
+public class Room() : Corridor {
 
     public Room(Vector2 topLeft, Vector2 bottomRight) : this() {
-        _topLeft = topLeft;
-        _bottomRight = bottomRight;
-        _floorTerrain = Terrain.OrcFloor;
-        _wallTerrain = Terrain.OrcWall;
-        _grid = new Dictionary<Vector2, Tile>();
+        TopLeft = topLeft;
+        BottomRight = bottomRight;
+        FloorTerrain = Terrain.OrcFloor;
+        WallTerrain = Terrain.OrcWall;
+        Grid = new Dictionary<Vector2, Tile>();
         Build();
     }
     
-    public bool ContainsPosition(Vector2 position) {
-        return position.X >= _topLeft.X && position.X <= _bottomRight.X &&
-               position.Y >= _topLeft.Y && position.Y <= _bottomRight.Y;
-    }
-
     private void Build() {
-        HashSet<Vector2> positions = RoomDigger.GetTileRegion(_topLeft, _bottomRight);
+        HashSet<Vector2> positions = RoomDigger.GetTileRegion(TopLeft, BottomRight);
         foreach (Vector2 pos in positions) {
-            _grid[pos] = Tile.CreateTileForTerrain(_floorTerrain);
+            Grid[pos] = Tile.CreateTileForTerrain(FloorTerrain);
         }
 
         List<Vector2> wallPositions = GetEdgePositions(positions.ToList());
         foreach (Vector2 pos in wallPositions) {
-            _grid[pos] = Tile.CreateTileForTerrain(_wallTerrain);
+            Grid[pos] = Tile.CreateTileForTerrain(WallTerrain);
         }
     }
 
     private List<Vector2> GetEdgePositions(List<Vector2> positions) {
         List<Vector2> wallPositions = positions.Where(pos => {
-            return (int)pos.X == (int)_topLeft.X || 
-                   (int)pos.Y == (int)_topLeft.Y ||
-                   (int)pos.X == (int)_bottomRight.X ||
-                   (int)pos.Y == (int)_bottomRight.Y;
+            return (int)pos.X == (int)TopLeft.X || 
+                   (int)pos.Y == (int)TopLeft.Y ||
+                   (int)pos.X == (int)BottomRight.X ||
+                   (int)pos.Y == (int)BottomRight.Y;
         }).ToList();
         return wallPositions;
     }
 
-    public void Draw(SpriteBatch spriteBatch) {
-        foreach ((Vector2 position, Tile tile) in _grid) {
-            tile.Draw(spriteBatch, position);
-        }
-    }
-
-    public Tile GetTileAt(Vector2 position) {
-        return _grid[position];
-    }
-
     public List<Vector2> GetTilePositions() {
-        return _grid.Keys.ToList();
+        return Grid.Keys.ToList();
     }
 
     public void AddDoorwayForCorridor(HashSet<Vector2> corridorTiles) {
-        List<Vector2> edgePositions = GetEdgePositions(_grid.Keys.ToList());
+        List<Vector2> edgePositions = GetEdgePositions(Grid.Keys.ToList());
         foreach (Vector2 position in edgePositions.Intersect(corridorTiles)) {
-            _grid[position] = Tile.CreateTileForTerrain(_floorTerrain);
+            Grid[position] = Tile.CreateTileForTerrain(FloorTerrain);
         }
     }
 }
